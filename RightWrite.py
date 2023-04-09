301740fc9461b012cc8a445d4865956e4681b8da
darkSteal1152 <89552151+darkSteal1152@users.noreply.github.com>
2023-04-09 18:53:40 -0500

    Create RightWrite.py

---
+++ RightWrite.py
@@ -0,0 +1,18 @@
+from easy_comms import Easy_comms
+from time import sleep
+from machine import Pin
+import json
+
+command = {'command': 'blink', 'args': 'on'}
+
+com1 = Easy_comms(0, 9600)
+led = Pin(25, Pin.OUT)
+
+while True:
+    com1.send(str(json.dumps(command)))
+    if command['args'] == 'on':
+        command = {'command': 'blink', 'args': 'off'}
+    else:
+        command = {'command': 'blink', 'args': 'on'}
+    led.toggle()
+    sleep(1)
