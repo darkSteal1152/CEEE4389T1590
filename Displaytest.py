56de3a24162dbec30c7bedf68dc3702a39f57c37
darkSteal1152 <89552151+darkSteal1152@users.noreply.github.com>
2023-04-13 19:08:34 -0500

    Update Displaytest.py

--- Displaytest.py
+++ Displaytest.py
@@ -3,8 +3,8 @@ from easy_comms import Easy_comms
 import time
 
 led = Pin('LED', Pin.OUT)
-button1 = Pin(14, Pin.OUT)
-button2 = Pin(15, Pin.OUT)
+#button1 = Pin(14, Pin.OUT)
+#button2 = Pin(15, Pin.OUT)
 n0 = Pin(18, Pin.OUT)
 n1 = Pin(19, Pin.OUT)
 n2 = Pin(20, Pin.OUT)
@@ -35,56 +35,56 @@ curr2 = 0
 com1 = Easy_comms(0, 9600)
 com2 = Easy_comms(1, 9600)
 
-'''
-def getMes():
-    com1 = Easy_comms(0, 9600)
-    
-    message = ""
-    message = com1.read()
-    
-    if message is not None:
-        #print(f"message received: {message.strip('\n')}")
-        com1.stop_read()
-        return int(message[-1])
-    
-    return False
-'''
+def inc(num):
+    num = (num + 1) % 10
+    return num
 
-while True:
-    
-    message1 = ""
-    message1 = com1.read()
-    
-    if message1 is not None:
-        num1 = int(message1[-1])
-        
-    time.sleep(0.1)
-    
-    message2 = ""
-    message2 = com2.read()
-    
-    if message2 is not None:
-        num2 = int(message2[-1])
-    
+def setDisplay1(num1):
     d0 = num1 % 2
     d1 = num1 // 2 % 2
     d2 = num1 // 4 % 2
     d3 = num1 // 8 % 2
-    
+
+    n0.value(d0)
+    n1.value(d1)
+    n2.value(d2)
+    n3.value(d3)
+
+def setDisplay2(num2):
     c0 = num2 % 2
     c1 = num2 // 2 % 2
     c2 = num2 // 4 % 2
     c3 = num2 // 8 % 2
     
-    n0.value(d0)
-    n1.value(d1)
-    n2.value(d2)
-    n3.value(d3)
-    
     m0.value(c0)
     m1.value(c1)
     m2.value(c2)
     m3.value(c3)
+
+def getMes1():
+    message1 = ""
+    message1 = com1.read()
+    
+    if message1 is not None:
+        print(f"message received: {message1.strip('\n')}")
+        return int(message1[-1])
+    
+    return False
+
+def getMes2():
+    message2 = ""
+    message2 = com2.read()
+    
+    if message2 is not None:
+        print(f"message received: {message2.strip('\n')}")
+        return int(message2[-1])
+    
+    return False
+
+setDisplay1(num1)
+setDisplay2(num2)
+
+while True:
     
     if num1 == num2:
         green.value(1)
@@ -97,32 +97,38 @@ while True:
         red.value(1)
         green.value(0)
     
-    if button1.value():
+    if getMes1():
         if prev1 != curr1:
             led.value(1)
             curr1 = 1
             time.sleep(0.2)
-            num1 = (num1 + 1) % 10
+            num1 = inc(num1)
             print("press", num1)
             
         else:
             led.value(0)
             curr1 = 0
             time.sleep(0.2)
-            num1 = (num1 + 1) % 10
+            num1 = inc(num1)
             print("unpress", num1)
+
+        setDisplay1(num1)
+
+    time.sleep(0.1)
         
-    if button2.value():
+    if getMes2():
         if prev2 != curr2:
             led.value(1)
             curr2 = 1
             time.sleep(0.2)
-            num2 = (num2 + 1) % 10
+            num2 = inc(num2)
             print("press2", num2)
             
         else:
             led.value(0)
             curr2 = 0
             time.sleep(0.2)
-            num2 = (num2 + 1) % 10
+            num2 = inc(num2)
             print("unpress2", num2)
+
+        setDisplay2(num2)
